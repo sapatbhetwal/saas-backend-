435689edb98f80f3952d3ebb16f06ed99bb9bb54
sapatbhetwal <bhetwalsapat@gmail.com>
2026-07-02 18:48:42 +0545

    Initial Spring Boot project setup

---
+++ src/main/java/io/github/sapatbhetwal/saas_backend/SaasBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.sapatbhetwal.saas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sapatbhetwal/saas_backend/SaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sapatbhetwal.saas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
