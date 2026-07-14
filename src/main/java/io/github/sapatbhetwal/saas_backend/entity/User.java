e4e4a7066bbe6326d8823ace77dbbc7c8fee3a08
sapatbhetwal <bhetwalsapat@gmail.com>
2026-07-14 18:42:51 +0545

    User

---
+++ src/main/java/io/github/sapatbhetwal/saas_backend/entity/User.java
@@ -0,0 +1,26 @@
+package io.github.sapatbhetwal.saas_backend.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // getters and setters
+}
+  
